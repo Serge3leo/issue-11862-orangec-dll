c2fdfa0de02c3b4f4c75d5c633f828fa714015b9
Serguei E. Leontiev <leo@sai.msu.ru>
2026-03-24 21:10:56 +0300

    Initial demo of `Compiler/OrangeC.cmake` bug(#1)

---
+++ run-build-test.bat
@@ -0,0 +1,20 @@
+@if "x%VERBOSE%"=="x" echo off
+rem vim:set sw=4 ts=8 et fileencoding=utf8:
+rem SPDX-License-Identifier: BSD-2-Clause
+rem SPDX-FileCopyrightText: 2026 Сергей Леонтьев (leo@sai.msu.ru)
+
+set build_type=Release
+set build_output_dir=build\win_occ
+cmake -B %build_output_dir% ^
+        -DCMAKE_C_COMPILER=occ ^
+        -DCMAKE_BUILD_TYPE=%build_type% ^
+        -G "MSYS Makefiles" ^
+        -S . %*
+if errorlevel 1 exit /b
+cmake --build %build_output_dir% --config %build_type%
+if errorlevel 1 exit /b
+ctest --output-on-failure --build-config %build_type% ^
+      --test-dir %build_output_dir%
+if errorlevel 1 exit /b
+%build_output_dir%\main-mnld.exe
+if errorlevel 1 exit /b
